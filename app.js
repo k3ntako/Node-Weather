231ad04ac5e82e6220f2ceceece81f972079da90
Kentaro Kaneki <k@Ken-2.local>
2018-11-30 23:30:11 -0500

    Axios

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode')
-const darksky = require('./darksky/darksky')
+const axios = require('axios')
 
 const argv = yargs
   .options({
@@ -17,12 +16,29 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address).then((location) => {
-  darksky.getWeather(location).then((weather) => {
-    console.log(weather);
-  }, (errorMessage) =>{
-    console.log(errorMessage);
-  });
-}, (errorMessage) =>{
-  console.log(errorMessage);
-});
+let encodedAddress = encodeURIComponent(argv.address);
+let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBJts_vCVZaXEgafo3It7bqc8_tGwr4ibM&address=${encodedAddress}`;
+
+axios.get(geocodeURL).then((response) => {
+  if(response.data.status === "ZERO_RESULTS"){
+    throw new Error('Unable to find that address.')
+  }
+  let latitude = response.data.results[0].geometry.location.lat
+  let longitude = response.data.results[0].geometry.location.lng
+  let weatherUrl =
+  `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}?units=auto&exclude=[minutely,hourly,daily,flags,alerts]`
+
+  console.log(response.data.results[0].formatted_address);
+
+  return axios.get(weatherUrl)
+}).then((response) => {
+  let summary = response.data.currently.summary;
+  let temp = response.data.currently.temperature;
+  console.log(`Currently: ${summary}, ${temp}˚F.`);
+}).catch((e) => {
+  if(e.message){
+    console.log(e.message);
+  }else{
+    console.log("Could not reach Google Maps API.");
+  }
+})
